fe58924c1939d73a1694fab27a5b6ce4bf48cbc5
Victor Smith <victorj500@gmail.com>
2022-01-08 10:46:00 -0600

    installable

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
 const path = require("path");
 const webpack = require("webpack");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 module.exports = {
-    devServer: {
+  devServer: {
     static: {
-      directory: __dirname
-    } 
+      directory: __dirname,
+    },
   },
   entry: {
     app: "./assets/js/script.js",
     events: "./assets/js/events.js",
     schedule: "./assets/js/schedule.js",
-    tickets: "./assets/js/tickets.js"
+    tickets: "./assets/js/tickets.js",
   },
   output: {
     filename: "[name].bundle.js",
@@ -24,32 +26,49 @@ module.exports = {
         test: /\.jpg$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
               esModule: false,
-              name (file) { 
-                return "[path][name].[ext]"
+              name(file) {
+                return "[path][name].[ext]";
               },
-              publicPath: function(url) {
-                return url.replace("../", "/assets/")
-              }
-            }
+              publicPath: function (url) {
+                return url.replace("../", "/assets/");
+              },
+            },
           },
           {
-            loader: 'image-webpack-loader'
-          }
-        ]
-      }
-    ]
+            loader: "image-webpack-loader",
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new webpack.ProvidePlugin({
       $: "jquery",
-      jquery: "jquery"
+      jquery: "jquery",
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: "static", // the report outputs to an HTML file in the dist folder
-    })
+    }),
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      start_url: "../index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      fingerprints: false,
+      inject: false,
+      icons: [
+        {
+          src: path.resolve("assets/img/icons/icon-512x512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+      ],
+    }),
   ],
-  mode: 'development'
+  mode: "development",
 };
